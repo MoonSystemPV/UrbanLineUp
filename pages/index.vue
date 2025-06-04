dc3ad4169d046076a6c07a402f81b89766e7a9ba
Nacho <105531940+NachoOFC@users.noreply.github.com>
2025-06-04 16:49:41 -0400

    carpetas iniciales

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <p>hola</p>
+</template>
+
+<script>
+  export default {
+        name: 'index'
+        data(){
+
+
+        },
+        methods: {
+            // metodo para hacer algo
+        }
+  }
+</script>
+<style scoped>
+
+</style>
